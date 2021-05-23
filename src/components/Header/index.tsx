de3768e070ded2f9c477e7d5e7728627663a732b
Gontran Bezerra <gontranbezerra@gmail.com>
2021-05-23 15:49:23 -0300

    feat: back function on Hearder component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,35 @@
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex, Image, Link, LinkOverlay } from '@chakra-ui/react';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
+import { useRouter } from 'next/router';
+import React from 'react';
 
-interface HeaderProps {
-  icon?: string;
-}
+export default function Header() {
+  const router = useRouter();
+  const isRoot = router.asPath==='/' ? true: false;
+  // console.log('Header.router: ', router);
 
-export default function Header(props: HeaderProps) {
-  const { icon, ...rest } = props;
   return (
-    <Flex
-      maxW={1440}
-      minH={[50, 75, 100]}
-      mx="auto"
-      justifyContent="center"
-      // border="1px"
-      // borderColor="green"
-      {...rest}
-    >
-      <Flex alignItems="center" maxW={[100, 120, 185]}>
-        <Image src="img/logo.svg" alt="Worldtrip" />
+    <>
+      {!isRoot && <Link href={`/`}>
+        <ChevronLeftIcon
+          w={[4, 6, 6, 8]}
+          h={[4, 6, 6, 8]}
+          position="absolute"
+          minH={[50, 75, 100]}
+          ml={[4, 16, 24, 36]}
+        />
+      </Link>}
+      <Flex
+        maxW={1440}
+        minH={[50, 75, 100]}
+        mx="auto"
+        justifyContent="center"
+        direction="row"
+      >
+        <Flex alignItems="center" maxW={[100, 120, 185]}>
+          <Image src="img/logo.svg" alt="Worldtrip" />
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
